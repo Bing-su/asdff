b423c705170d7aec41c87a678e36470009adc073
Bingsu <ks2515@naver.com>
2023-07-25 21:10:31 +0900

    fix: don't fix inpaint num images

--- asdff/base.py
+++ asdff/base.py
@@ -147,6 +147,5 @@ class AdPipelineBase(ABC):
         return {
             **common,
             **inpaint_only,
-            "num_images_per_prompt": 1,
             "output_type": "pil",
         }
